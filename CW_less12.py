5cfa98de1f8ac654cf99efdfaab40ccff0e2870d
ksu_gorska <ksu.gorska@gmail.com>
2025-11-17 10:18:03 +0200

    Add files via upload

---
+++ CW_less12.py
@@ -0,0 +1,42 @@
+import pandas as pd # таблиці
+import numpy as np
+import tensorflow as tf # для нейронок
+from tensorflow import keras
+from tensorflow.keras import layers # шари
+from sklearn.preprocessing import LabelEncoder # перероблює текст в числа
+import matplotlib.pyplot as plt # побудова графіків
+
+# 2.робота з ссв файлом
+df = pd.read_csv('data/figures.csv')
+# print(df.head())
+encoder = LabelEncoder()
+df['label_enc'] = encoder.fit_transform(df['label'])
+
+# 3.обрали елементи для навчання
+X = df[['area', 'perimeter', 'corners']]
+y = df['label_enc']
+
+# 4.створення моделі
+model = keras.Sequential([layers.Dense(8, activation='relu', input_shape=(3, )),
+                          layers.Dense(8, activation='relu'),
+                          layers.Dense(8, activation='softmax')])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# 5.навчання
+history = model.fit(X, y, epochs = 200, verbose = 0)
+
+# 6.візуалізація навчання
+plt.plot(history.history['loss'], label = "Втрата")
+plt.plot(history.history['accuracy'], label = "Точність")
+plt.xlabel('епоха')
+plt.ylabel('значення')
+plt.title("процес навчання")
+plt.legend()
+plt.show()
+
+# 7.тестування
+test = np.array([18, 16, 0])
+
+pred = model.predict(test)
+print(f'Іймовірність кожного класу: {pred}')
+print(f'модель визначила: {encoder.inverse_transform(np.argmax(pred))}')
